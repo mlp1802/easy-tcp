81727d06d7c2dc743ec1f2044d89cab9d2ebb97c
Mikkel Petersen <mlp2305@gmail.com>
2016-06-20 00:27:36 +0200

    updated example

--- example.js
+++ example.js
@@ -1,20 +1,20 @@
-  connect= require('./easy-tcp').connect
+  connect = require('./easy-tcp').connect
   listen = require('./easy-tcp').listen
   port = 3000;
   //Server
   listen(port, function(client){
     console.log("Client connected");
-    return client.on("chat", function(m){
-      return client.send({
-        _name: "chat",
-        text: "Got your message buddy !"
-      });
+    client.on("chat", function(m){
+        client.send({
+          _name: "chat",
+          text: "Got your message buddy !"
+        });
     });
   });
   //Client
   connect("localhost", port, function(client){
     client.on("chat", function(message){
-      return console.log("Chat message: " + message.text);
+       console.log("Chat message: " + message.text);
     });
     return client.send({
       _name: "chat",
